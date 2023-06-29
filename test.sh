b4f3ab8375c965c1f3e7aff7941f1838e4c9970f
F.List <36734596+flistell@users.noreply.github.com>
2023-06-29 13:46:31 +0000

    added prefer-application-packages

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 
 echo "### 1. Plain JAX-RS and JSON-B annotations (PLainJaxRSApplication.java) ###"
-curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources/datedemo | jq
+curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources/datedemo | jq .date
 
 echo "### 2. Use of Jackson registered in JaxRsWithJaksonApplication.java ###"
-curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources2/datedemo | jq
+curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources2/datedemo | jq .date
 
 echo "### 3. Use of Jackson configured in web.xml ###"
-curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources3/datedemo | jq
+curl -qs -x '' http://localhost:7001/recordrest-web-jackson/resources3/datedemo | jq .date
